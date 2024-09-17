18712c709efec7f4004d37272078a37c58588559
TTHiagoADS <thiagoubatuba123@gmail.com>
2024-09-17 19:57:56 -0300

    implements user delete

--- src/Controllers/DetailUserController.ts
+++ src/Controllers/DetailUserController.ts
@@ -2,7 +2,13 @@ import {Request, Response} from "express"
 import {DetailUserService} from "../../src/Services/User/DetailUserService"
 
 class DetailUserController{
-    
-}
 
-export {DetailUserController}
+    async handle(request: Request, response:Response){
+        const user_id = request?.user_id;
+        const detailUserService = new DetailUserService();
+        const user = await detailUserService.execute(user_id);
+        response.json(user);
+
+ }
+}
+export {DetailUserController};
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,9 @@
 import {Router, Request, Response, request } from "express"
 import { CreateUserController } from "./Controllers/User/CreateUserController";
 import { AuthUserController } from "./Controllers/User/AuthUserController";
+import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { DetailUserController } from "./Controllers/DetailUserController";
+import { RemoveUserController } from "./Controllers/User/RemoveUserController";
 
 const router = Router();
 router.get("/test",(request: Request, response:Response) => {
@@ -8,7 +11,9 @@ router.get("/test",(request: Request, response:Response) => {
 });
 
 //User routes
-router.post('/user', new CreateUserController().handle)
-router.post('/session', new AuthUserController().handle)
+router.post('/user', new CreateUserController().handle) //cria um usuario
+router.post('/session', new AuthUserController().handle) //obtem o token
+router.get('/me', isAuthenticated, new DetailUserController().handle) //encontra o id do usuario
+router.delete("/user/remove", new RemoveUserController().handle) //remove o ususario pelo id
 
 export {router};
